418bda4ae9ca14d79046559481d8ea5960022f7c
danya717 <daniel.shv00@gmail.com>
2025-07-26 10:39:37 +0300

    Refactored js logic. Simplified right answer check

--- script.js
+++ script.js
@@ -5,47 +5,31 @@ let answerOfQust4 = "1 year"
 let count = 0
 
 document.querySelector(".checkBttn").onclick = function() {
-    alert("You didn't answer to all questions!")
     document.querySelector(".checkBttn").disabled = true;
     let userInput1 = document.querySelector(".qustInput1").value;
     if (userInput1 === answerOfQust1) {
+        count += 1
         console.log("CorrectAnsw1")
     }
     let userInput2 = document.querySelector(".qustInput2").value;
     if (userInput2 === answerOfQust2) {
+        count += 1
         console.log("CorrectAnsw2")
     }
     let userInput3 = document.querySelector(".qustInput3").value;
     if (userInput3 === answerOfQust3) {
+        count += 1
         console.log("CorrectAnsw3")
     }
     let userInput4 = document.querySelector(".qustInput4").value;
     if (userInput4 === answerOfQust4) {
+        count += 1
         console.log("CorrectAnsw4")
     }
-    if (userInput1 === answerOfQust1) {
-        if (userInput2 === answerOfQust2) {
-            if (userInput3 === answerOfQust3) {
-                if (userInput4 === answerOfQust4) {
-//                    count += 1
-                    alert("You have all answers correct")
-                    console.log("4 are correct")
-                }
-            }
-        }
+    if (count === 4) {
+        alert("You have all the answers correct")
     }
-    if (userInput1 === answerOfQust1) {
-        count += 1
-    }
-    if (userInput2 === answerOfQust2) {
-        count += 1
-    }
-    if (userInput3 === answerOfQust3) {
-        count += 1
-    }
-    if (userInput4 === answerOfQust4) {
-        count += 1
-    }
+
     if (count === 1) {
         alert("You have 1 correct");
     }
